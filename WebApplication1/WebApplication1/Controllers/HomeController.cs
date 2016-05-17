33af5fb8f8caad4203161a6e44b422f9eab30d1e
APO <ringo1625@gmail.com>
2016-05-17 10:24:44 +0900

    最初のコミット

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kekka(int hensu1)
+        {
+            if (hensu1 > 10)
+            {
+                ViewBag.Kotae = "10以上";
+            }
+            else
+            {
+                ViewBag.Kotae = "10以下";
+            }
+
+            return View();
+        }
+    }
+}
